802bf3d89f7a60ecac459552a79034503d605699
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-02 21:55:44 +0100

    Brew formula update for humio version v0.15.0

--- Formula/humio.rb
+++ Formula/humio.rb
@@ -1,9 +1,9 @@
 class Humio < Formula
   desc "Manage and Stream Logs to Humio"
   homepage "https://humio.com/"
-  url "https://github.com/humio/cli/releases/download/v0.14.0/humio_0.14.0_macOS_64-bit.tar.gz"
-  version "0.14.0"
-  sha256 "36cece297f53272b2cd6f5145ff666b4364b1ea127122108b020486fb98216a6"
+  url "https://github.com/humio/cli/releases/download/v0.15.0/humio_0.15.0_macOS_64-bit.tar.gz"
+  version "0.15.0"
+  sha256 "00b9da6d7f90afc7c73ae0337c956c7d468dc94496c1837af1d949d44bccfd12"
 
   def install
     bin.install "humio"
