be97f89329382f81ffe732b0b51c776a242059a0
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-11 08:46:46 +0100

    Brew formula update for humioctl version v0.23.0

--- Formula/humioctl.rb
+++ Formula/humioctl.rb
@@ -1,9 +1,9 @@
 class Humioctl < Formula
   desc "Manage and Stream Logs to Humio"
   homepage "https://humio.com/"
-  url "https://github.com/humio/cli/releases/download/v0.22.0/humioctl_0.22.0_macOS_64-bit.tar.gz"
-  version "0.22.0"
-  sha256 "9435dca6954d37b60ce0c9a7117d39547c6c63075f775c33f087acab34d9463b"
+  url "https://github.com/humio/cli/releases/download/v0.23.0/humioctl_0.23.0_macOS_64-bit.tar.gz"
+  version "0.23.0"
+  sha256 "13163117d0797e5b994820dc7ad59f36e817b0a2a2188f99cdce297a337e2480"
 
   def install
     bin.install "humioctl"
