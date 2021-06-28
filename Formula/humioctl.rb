089a063b485be50ac62b39e4bdd629a2cdd8475a
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-28 09:31:57 +0200

    Brew formula update for humioctl version v0.28.4

--- Formula/humioctl.rb
+++ Formula/humioctl.rb
@@ -5,16 +5,21 @@
 class Humioctl < Formula
   desc "Manage and Stream Logs to Humio"
   homepage "https://humio.com/"
-  version "0.28.3"
+  version "0.28.4"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/humio/cli/releases/download/v0.28.3/humioctl_0.28.3_macOS_64-bit.tar.gz"
-    sha256 "0ec57992e8f814cb6b2a65804bc90064c9f236ddf16210a8e4639fbdfa11d52d"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/humio/cli/releases/download/v0.28.4/humioctl_0.28.4_macOS_64-bit.tar.gz"
+      sha256 "37dd29aaafb6fdd0a4decb634db2f2aefadd97c0d4636979c7b3bd4e14f5b477"
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/humio/cli/releases/download/v0.28.3/humioctl_0.28.3_Linux_64-bit.tar.gz"
-    sha256 "9c55215d545a7701b48d17fea50b9f32d414b0960721b80ba6969e6899c36b59"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/humio/cli/releases/download/v0.28.4/humioctl_0.28.4_Linux_64-bit.tar.gz"
+      sha256 "98855f34a2b8c5367e442d8f60f6a67cb748bb0ba0a633540e9c8c5724a4ce21"
+    end
   end
 
   def install
