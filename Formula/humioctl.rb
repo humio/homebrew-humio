99587ba2079ffbaa5046522eb8e2f67066fd4f6a
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-29 13:46:23 +0200

    Brew formula update for humioctl version v0.28.5

--- Formula/humioctl.rb
+++ Formula/humioctl.rb
@@ -5,20 +5,20 @@
 class Humioctl < Formula
   desc "Manage and Stream Logs to Humio"
   homepage "https://humio.com/"
-  version "0.28.4"
+  version "0.28.5"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/humio/cli/releases/download/v0.28.4/humioctl_0.28.4_macOS_64-bit.tar.gz"
-      sha256 "37dd29aaafb6fdd0a4decb634db2f2aefadd97c0d4636979c7b3bd4e14f5b477"
+      url "https://github.com/humio/cli/releases/download/v0.28.5/humioctl_0.28.5_macOS_64-bit.tar.gz"
+      sha256 "7afa1eb001045f0a1bdbb10eea576cfeddbb667d682ca1dcfce20fc73cbead1b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/humio/cli/releases/download/v0.28.4/humioctl_0.28.4_Linux_64-bit.tar.gz"
-      sha256 "98855f34a2b8c5367e442d8f60f6a67cb748bb0ba0a633540e9c8c5724a4ce21"
+      url "https://github.com/humio/cli/releases/download/v0.28.5/humioctl_0.28.5_Linux_64-bit.tar.gz"
+      sha256 "51c2b0fcf00ee39daec45709ee0662bf2482646ca6a55c3d0af67d1af469ab05"
     end
   end
 
